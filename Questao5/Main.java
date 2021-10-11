8dec6096b5d0156bd68dfd068814b0d3cc40dcfc
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-11 09:35:34 -0300

    Create Main.java

---
+++ Questao5/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Contato contato = new Contato("Maria");
+
+        System.out.println();
+        contato.recebeMensagem("Oi");
+
+        System.out.println();
+        System.out.println("Voce tem " + contato.getQuantidadeDeMensagensNaoLidas() + " mensagens nao lidas com "
+                + contato.getNome() + "!");
+
+        System.out.println();
+        contato.enviaMensagem("Olá.");
+        contato.enviaMensagem("Como você está?");
+        contato.enviaMensagem("Tudo bem com a família?");
+        contato.enviaMensagem("Saudades.");
+        contato.enviaMensagem("Te vejo por ai!");
+
+
+        System.out.println();
+        System.out.println("Voce tem " + contato.getQuantidadeDeMensagensNaoLidas() + " mensagens não lidas com "
+                + contato.getNome() + "!");
+
+      
+        System.out.println();
+        contato.mostrarMensagensNaoLidas();
+
+     
+        System.out.println();
+        System.out.println("Voce tem " + contato.getQuantidadeDeMensagensNaoLidas() + " mensagens não lidas com "
+                + contato.getNome() + "!");
+
+    }
+}

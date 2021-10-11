ffa90c5200046a693aecc5bc4e148f1bd0e0085b
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-11 09:23:06 -0300

    Create Aviao.java

---
+++ Questao3/Aviao.java
@@ -0,0 +1,32 @@
+public class Aviao {
+	
+	    private int altitude;
+      private int velocidade;
+   
+   public Aviao(int altitude, int velocidade) {
+    this.altitude = altitude;
+    this.velocidade = velocidade;
+   }
+ 
+ public int getAltitude() {
+ return altitude;
+ }
+ public void setAltitude(int altitude) {
+ this.altitude = altitude;
+ }
+ public int getVelocidade() {
+ return velocidade;
+ }
+ public void setVelocidade(int velocidade) {
+ this.velocidade = velocidade;
+ }
+
+ public void reduzirAltitude(int altitude) {
+ this.altitude = this.getAltitude()- altitude;
+ }
+public void reduzirVelocidade(int velocidade) {
+this.velocidade = this.getVelocidade() - velocidade;
+ }
+}
+
+  

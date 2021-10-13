332ff596e2b8ceecebb57079d5d6f981ecdbdd13
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-10-13 12:06:30 -0300

    Update Main.java

--- Questao3/Main.java
+++ Questao3/Main.java
@@ -1,6 +1,6 @@
 public class Main{ 
-	  public static void main(String[] args) {
-	  Aviao aviao = new Aviao();
+    public static void main(String[] args) {
+    Aviao aviao = new Aviao();
     aviao.setAltitude(10);
     aviao.setVelocidade(900);
   
@@ -15,6 +15,5 @@ System.out.println("                             ");
   System.out.println ("Altitude atual:"+aviao.getAltitude()+"Km");
   System.out.println ("Velocidade atual:"+aviao.getVelocidade()+"Km/h");
  
-
     }
   }
